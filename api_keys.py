5be30ce0b4ef376c0c01431046952a57403072f2
Alex121619 <at.ghebrezghi@gmail.com>
2023-03-10 01:47:35 +0000

    First commit

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+#weather_api_key = "YOUR KEY HERE"
+weather_api_key = "80913234e69296e45dd43131c22cd2ae"
+# Geoapify API Key
+#geoapify_key = "YOUR KEY HERE"
+geoapify_key = "5dd73a9962f845fa9b609118075a1ced"
